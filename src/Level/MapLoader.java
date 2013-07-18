2727365bba2ff281e94faf493472f9b047374970
Oramy <ymarua@hotmail.fr>
2013-07-18 13:46:20 +0200

    MapLoader updated

--- src/Level/MapLoader.java
+++ src/Level/MapLoader.java
@@ -52,4 +52,74 @@ public class MapLoader {
 		}
 		return map;
 	}
+	public static final boolean isExisting(String path){
+		File f = new File(path);
+		return f.exists();
+	}
+	public static final void clearAutoSaves(String path){
+		for(int i = 0; i < 9; i++){
+			path = path.substring(0, path.length() - 3)+"$"+i+".dat";
+			File f =  new File(path);
+			if(f.exists()){
+				f.delete();
+			}
+		}
+	}
+	public static final void clearAllAutoSaves(){
+		for(int i = 0; i < 9; i++){
+			File f =  new File("data/Maps/");
+		}
+	}
+	public static final boolean hasAutoSave(String path){
+		int autoSave =  1;
+		path = path.substring(0, path.length() - 3)+"$"+autoSave+".dat";
+		File f =  new File(path);
+		while(!f.exists() && autoSave <= 9){
+			path = path.substring(0, path.length() - 3)+"$"+autoSave+".dat";
+			f =  new File(path);
+			autoSave++;
+		}
+		return f.exists();
+	}
+	public static final File getAutoSaveFile(String path){
+		int autoSave =  1;
+		path = path.substring(0, path.length() - 3)+"$"+autoSave+".dat";
+		File f =  new File(path);
+		while(!f.exists() && autoSave <= 9){
+			path = path.substring(0, path.length() - 3)+"$"+autoSave+".dat";
+			f =  new File(path);
+			autoSave++;
+		}
+		return f;
+	}
+	public static final ChunkMap loadAutoSavedMap(String path){
+		ObjectInputStream ois;
+		
+		File f = getAutoSaveFile(path);
+		
+		ChunkMap map = new ChunkMap(0, 0, 0, 0);
+		
+		if(f.exists()){
+			try {
+				ois = new ObjectInputStream(
+						new BufferedInputStream(
+								new FileInputStream(f)));
+				
+				try {
+					map = (ChunkMap) ois.readObject();
+					
+					ois.close();
+					return map;
+				} catch (ClassNotFoundException e) {
+					
+					e.printStackTrace();
+				}
+				ois.close();
+			} catch (IOException e) {
+				
+				e.printStackTrace();
+			}
+		}
+		return map;
+	}
 }
